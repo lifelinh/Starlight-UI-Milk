e59a0b2a52b4c5b0ff0f37aa0489a8d0a7a35e78
bbbPriv <elcuentabypass10@gmail.com>
2026-04-19 16:35:14 +0200

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -122,6 +122,11 @@ local MarketplaceService = GetService("MarketplaceService")
 local ReplicatedStorage = GetService("ReplicatedStorage")
 local ContentProvider = GetService("ContentProvider")
 local CoreGui = GetService("CoreGui")
+local InputManager
+
+if not getgenv().SecureMode then
+	InputManager = GetService("VirtualInputManager")
+end
 
 local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui")
@@ -1522,7 +1527,27 @@ local function Hide(Interface, JustHide: boolean?, Notify: boolean?, Bind: strin
 		end
 	end
 
-
+	-- hide popups
+	if InputManager then
+		if not isStudio and Starlight.Instance.MobileToggle.Visible then
+			InputManager:SendTouchEvent(
+				0, 0, 0, 0
+			)
+	
+			InputManager:SendTouchEvent(
+				0, 2, 0, 0
+			)
+		elseif not isStudio then
+			InputManager:SendMouseButtonEvent(
+				0, 0, 0, true, game, 0
+			)
+	
+			InputManager:SendMouseButtonEvent(
+				0, 0, 0, false, game, 0
+			)
+		end
+	end
+	
 	task.wait(0.18)
 	if Interface.ClassName == "ScreenGui" then
 		Interface.Enabled = false
